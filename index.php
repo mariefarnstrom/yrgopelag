d011057d1f3ae2df9da8b1b3d454d233578e3c8e
Marie <fernstrm@hotmail.com>
2025-12-12 11:38:07 +0100

    added style.css and booking form

--- index.php
+++ index.php
@@ -8,7 +8,35 @@ $dotenv->load();
 $apiKey = $_ENV['API_KEY'] ?? null;
 
 require __DIR__ . '/views/header.php';
+?>
+
+<main>
+    <form class="bookingForm" action="" method="post">
+        <label for="">Name</label>
+        <input type="text">
+
+        <label for="">Transfer code</label>
+        <input type="text">
+
+        <label for="">Arrival</label>
+        <input type="date">
+
+        <label for="">Departure</label>
+        <input type="date">
+       
+        <label>Room type</label>
+        <select>
+            <option value="economy">Economy</option>
+            <option value="standard">Standard</option>
+            <option value="luxury">Luxury</option>
+        </select>
+        <input type="submit" value="Submit">
+        
 
 
 
+    </form>
+    <label for=""></label>
+</main>
+<?php
 require __DIR__ . '/views/footer.php';
--- views/header.php
+++ views/header.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Outset Shores Hotel</title>
+    <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
     
