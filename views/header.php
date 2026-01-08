ccdd982b3228d309da662f93d0b1f72a0999c0a4
Marie <fernstrm@hotmail.com>
2026-01-08 15:20:50 +0100

    changed heading font

--- views/header.php
+++ views/header.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="../assets/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 <body>
     
